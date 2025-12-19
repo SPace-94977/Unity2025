09ff25c6fca471edc77b398341b5a04978bdf65b
SPace-94977 <samuel.pace.j94977@mcast.edu.mt>
2025-12-19 18:25:13 +0100

    Space Shooter Commit 2
    - Added Main Menu functionality
    - Added Paths 2+3, not being followed by enemies yet

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("Level");
     }
 
     public void QuitGame()
