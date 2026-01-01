417fd79df6327f134c95e8231f9154f8368f6403
SPace-94977 <samuel.pace.j94977@mcast.edu.mt>
2026-01-01 16:40:01 +0100

    Space Shooter Commit 9
    - Implemented current Health display
    - [TO FIX] Player randomly taking 50 damage on game start

--- Assets/Script/Health.cs
+++ Assets/Script/Health.cs
@@ -1,8 +1,17 @@
 using UnityEngine;
+using TMPro;
 
 public class Health : MonoBehaviour
 {
     [SerializeField] int health = 100;
+    TMP_Text healthText;
+
+    private void Start()
+    {
+        // Same system as Points display, finding Health object of type TMP_Text
+        healthText = GameObject.Find("Health").GetComponent<TMP_Text>();
+        UpdateHealthText();
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -17,9 +26,19 @@ public class Health : MonoBehaviour
     void TakeDamage(int damage)
     {
         health -= damage;
+        if (health < 0)
+            health = 0;
+
+        UpdateHealthText();
+
         if (health <= 0)
         {
             Destroy(gameObject);
         }
+    }
+
+    void UpdateHealthText()
+    {
+        healthText.text = health + " / 100";
     }
 }
